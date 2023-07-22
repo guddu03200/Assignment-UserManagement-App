fa99a738ad784960d44f73be7a2ce3a2f7372139
Guddu Kumar <guddukumar032002@gmail.com>
2023-07-22 15:10:04 +0530

    assignment user management

---
+++ UserManagement-App/src/main/java/com/Guddu/UserManagementApp/configuration/BeanManager.java
@@ -0,0 +1,18 @@
+package com.Guddu.UserManagementApp.configuration;
+
+import com.Guddu.UserManagementApp.model.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class BeanManager {
+
+    @Bean
+    public List<User> getUsers(){
+
+        return new ArrayList<>();
+    }
+}
